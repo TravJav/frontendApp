fe5940e1e07f856deac5efb98b18fa790187f3e0
TravJav <travishaycock92@gmail.com>
2018-01-21 20:06:07 -0500

    Google maps implemented

--- Screens/DashScreens/Findgym.js
+++ Screens/DashScreens/Findgym.js
@@ -1,7 +1,6 @@
 import store from 'react-native-simple-store';   //https://www.npmjs.com/package/react-native-simple-store
 import React from 'react';
-
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
+import MapView, { PROVIDER_GOOGLE, Marker, Callout } from 'react-native-maps'
 import { CreateStore, applyMiddleware } from 'redux';
 import {
     StyleSheet,
@@ -14,30 +13,76 @@ import {
     AppRegistry,
     ImageBackground
 } from 'react-native';
-
-
-
 import {
     StackNavigator,
     TabNavigator,
 } from 'react-navigation';
+
+const MAP_KEY = "AIzaSyAdJxnlhQgWeZM6aAzsDhDIrHtd3CMQnDA";
+
+
 var Promise = require('bluebird');
 
 import Button from 'react-native-button';
+
+
 
 export default class Findgym extends React.Component {
 
     constructor() {
         super();
-
-
         this.state = {
             mapRegion: null,
             lastLat: null,
             lastLong: null,
-          }
-     
+            places: null,
+            long: null,
+            lat: null
+           
+        }
     }
+
+   findGym(){
+    const search = this.getUrWithParams(this.state.lastLat,this.state.lastLong, 2000, 'gym','gym', MAP_KEY)
+    console.log(search)
+    fetch(search)
+    .then((data) => data.json())
+    .then((res) => {
+      console.log(res);
+      const arrayMarkers = [];
+      res.results.map((element, i) => {
+        arrayMarkers.push(
+        <Marker
+         key={i}
+         coordinate={{
+           latitude: element.geometry.location.lat,
+           longitude: element.geometry.location.lng
+         }}
+        >
+         <Callout>
+           <View>
+             <Text>{element.name}</Text>
+            <Text> Rating:{element.rating}</Text>
+            <Text> Hours: {element.open_now}</Text>
+             </View>
+             </Callout>
+             </Marker>
+        )
+      })
+      this.setState({places: arrayMarkers});
+    })
+   }
+  
+
+   getUrWithParams(lat, long, radius, type, name, API){
+    const url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?";
+    const location =  `location=${lat},${long}&radius=${radius}`;
+    const typeData = `&types=${type}`;
+    const typeName = `&name=${name}`;
+    const key = `&key=${API}`;
+    return `${url}${location}${typeData}${typeName}${key}`;
+   }
+
     componentDidMount() {
         this.watchID = navigator.geolocation.watchPosition((position) => {
           // Create the object to update this.state.mapRegion through the onRegionChange function
@@ -49,6 +94,9 @@ export default class Findgym extends React.Component {
           }
           this.onRegionChange(region, region.latitude, region.longitude);
         });
+          setTimeout(() => {
+           this.findGym(); // Find the gyms close to user
+          }, 8000);
       }
 
       componentWillUnmount() {
@@ -63,10 +111,37 @@ export default class Findgym extends React.Component {
           lastLong: lastLong || this.state.lastLong
         });
       }
+
     static navigationOptions = {
         title: 'Find A Gym',
     };
+
     render() {
+      return (
+        <View style={{flex: 1}}>
+          <MapView
+            style={styles.map}
+            region={this.state.mapRegion}
+            showsUserLocation={true}
+            followUserLocation={true}
+            onRegionChange={this.onRegionChange.bind(this)}>
+      
+           {this.state.places}
+          </MapView>
+        </View>
+      );
+    }
+  }
+  
+const styles = StyleSheet.create({
+    map: {
+      ...StyleSheet.absoluteFillObject,
+    },
+  });
+
+  /*
+
+   render() {
     return (
       <View style={{flex: 1}}>
         <MapView
@@ -83,17 +158,48 @@ export default class Findgym extends React.Component {
             <View>
               <Text style={{color: '#000'}}>
                 { this.state.lastLong } / { this.state.lastLat }
+
               </Text>
             </View>
+            {this.state.places}
           </MapView.Marker>
+          
         </MapView>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-    map: {
-      ...StyleSheet.absoluteFillObject,
-    },
-  });
+  */
+
+  /*
+
+old 
+
+
+ render() {
+    return (
+      <View style={{flex: 1}}>
+        <MapView
+          style={styles.map}
+          provider={MapView.PROVIDER_GOOGLE}
+          initialRegion={{
+            latitude: this.state.lastLat,
+            longitude: this.state.lastLong
+          }}
+          >
+          <Marker
+coordinate={{
+  latitude: this.state.lastLat,
+  longitude: this.state.lastLong
+}}
+/>
+    {this.state.places}
+      </MapView>
+      </View>
+    );
+  }
+}
+
+
+  */
